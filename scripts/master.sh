848d2b641ff9f9d86329a846052233629166e796
Jung Eun Woo <realplord@gmail.com>
2026-05-17 12:15:38 +1200

    chore: update pod network CIDR and replace Flannel with Calico CNI in cluster initialization script

--- scripts/master.sh
+++ scripts/master.sh
@@ -8,15 +8,16 @@ echo ">>> Pre-pulling Kubernetes images (this may take a few minutes)..."
 sudo kubeadm config images list | grep -v "Finished" | xargs -I {} -P 4 sudo crictl pull {}
 
 echo ">>> Initializing Kubernetes cluster on ${IPADDR}..."
-sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=${IPADDR}
+sudo kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address=${IPADDR}
 
 echo ">>> Setting up kubeconfig for $(whoami)..."
 mkdir -p $HOME/.kube
 sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-echo ">>> Installing Flannel CNI..."
-kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
+echo ">>> Installing Calico CNI..."
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.3/manifests/tigera-operator.yaml
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.3/manifests/custom-resources.yaml
 
 echo ">>> Generating join command for workers..."
 kubeadm token create --print-join-command > join-command.sh
